4ba9a6c3a2e1b3c2f15e434b42804549507ceaa9
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-03-23 17:09:57 -0700

    find index function test pass w/ -1

--- find-index.js
+++ find-index.js
@@ -2,7 +2,8 @@ const likeFindIndex = (array, callback) => {
   for (let i = 0; i < array.length; i++) {
     if (callback(array[i]))  
     return i;
-}  
+    }
+    return -1; 
 }
 
 
--- find-index.test.js
+++ find-index.test.js
@@ -5,4 +5,8 @@ describe('find index', () => {
     it('returns the first element in the array that satisfies the callback function', () => {
      expect(likeFindIndex([3, 6, 9, 12, 15], (n) => n > 10)).toEqual(3);   
     });
+
+    it('returns the first element in the array that satisfies the callback function', () => {
+     expect(likeFindIndex([3, 4, 5, 6, 7], (n) => n > 10)).toEqual(-1);   
+    });
 });
